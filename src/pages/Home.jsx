e25fcded6cd1f8fe56fe2c6f48f84215a495305b
developerbarun <100085303+developerbarun@users.noreply.github.com>
2022-11-05 12:31:25 +0530

    First commit 12:30PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import Announcement from '../components/Announcement'
-import Categories from '../components/Categories'
 import NavBar from '../components/NavBar'
-import Slider from '../components/Slider'
 
 const Home = () => {
   return (
     <div>
       <Announcement/>
       <NavBar/>
-      <Slider/>
-      {/* <Categories/> */}
     </div>
   )
 }
